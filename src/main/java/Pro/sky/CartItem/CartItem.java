47b14a812e8b8617fdc0ae18a57492cf73c22918
1XSTRY <kosogornov99@gmail.com>
2024-01-28 15:32:51 +0500

    initial commit

---
+++ src/main/java/Pro/sky/CartItem/CartItem.java
@@ -0,0 +1,10 @@
+package Pro.sky.CartItem;
+
+import org.springframework.boot.SpringApplication;
+public class CartItem {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartItem.class, args);
+	}
+
+}
