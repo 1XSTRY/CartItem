d9b07bef9696cd890df7046f48c86e2ac4064794
1XSTRY <kosogornov99@gmail.com>
2024-02-11 15:17:51 +0500

    fix SessionScope

---
+++ src/test/java/Pro/sky/CartItem/CartItemApplicationTests.java
@@ -0,0 +1,13 @@
+package Pro.sky.CartItem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartItemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
